be2eef93fc1d4d49849eec623c220c16572a20ad
24heures <24heures>
2017-04-17 08:02:13 +0200

    Gestion de la sélection dans la liste déroulante

--- avance-java/chapitre_6/js/pays.js
+++ avance-java/chapitre_6/js/pays.js
@@ -20,22 +20,35 @@ var listePays = [
     "Azerbaïdjan"
 ];
 
-//Affiche les suggestions
-function showSuggestions(listeSuggestions){
-    var paysElt = document.getElementById("pays");
-    var suggestionsElt = document.getElementById("suggestions");
-    //Tri les suggestions encore bonnes
-    for(var i =0; i< listeSuggestions.length; i++){
-        var suggestion = listeSuggestions[i];
-        if (suggestion.toLowerCase().indexOf(paysElt.value.toLowerCase()) !== -1){
-            var suggestionElt = document.createElement("div");
-            suggestionElt.id = "suggestion_"+i;
-            suggestionElt.setAttribute("classe", "suggestion");
-            suggestionElt.textContent = suggestion;
-            suggestionsElt.appendChild(suggestionElt);
+function supprChildNodes (node){
+     while (node.firstChild) {
+            node.removeChild(node.firstChild);
         }
-    }
 }
 
 var paysElt = document.getElementById("pays");
-paysElt.addEventListener("input", showSuggestions(listePays));
+paysElt.addEventListener("input", function (){
+    var suggestionsElt = document.getElementById("suggestions");
+    
+    //Supprime les suggestions précédentes
+    supprChildNodes(suggestionsElt);
+
+    //Tri les suggestions encore bonnes
+    for(var i =0; i< listePays.length; i++){
+        var suggestion = listePays[i];
+        if (suggestion.toLowerCase().indexOf(paysElt.value.toLowerCase()) !== -1){
+            var suggestionElt = document.createElement("div");
+            suggestionElt.id = "suggestion_"+i;
+            suggestionElt.setAttribute("class", "suggestion");
+            suggestionElt.textContent = suggestion;
+
+            //Supprime toutes les suggestions et met à jour la valeur du champ input
+            suggestionElt.addEventListener("click", function(e){
+                paysElt.value = e.target.textContent;
+                supprChildNodes(suggestionsElt);
+            })
+
+            suggestionsElt.appendChild(suggestionElt);
+        }
+    }
+});
